1a07d9084951bcb2f55cd7569236cadc8a66cbf3
nexta <gadressformuyuan@gmail.com>
2024-10-30 18:31:06 -0400

    text animation 1

--- sketch.js
+++ sketch.js
@@ -12,8 +12,12 @@ let lyrics =
     "beats": 32
   },
   {
+    "text": "■■■■■■",
+    "beats": 3
+  },
+  {
     "text": "■■■■■■も<br>言えないこんな<br>世の中じゃ",
-    "beats": 16
+    "beats": 13
   },
   {
     "text": "■の盃を<br>呷ったほうが<br>マシだね",
@@ -156,9 +160,44 @@ let accumulatedBeats = 0;
     }
 }
 
+// function showLyric(lyric) {
+//   if (lyric !== currentLyric) {
+//     currentLyric = lyric;
+//     lyricDiv.html('');
+//   }
+// }
+
 function showLyric(lyric) {
-if (lyric !== currentLyric) {
-    currentLyric = lyric;
-    lyricDiv.html(currentLyric);
-}
+  if (lyric !== currentLyric) {
+      currentLyric = lyric;
+
+      lyricDiv.html('');
+
+      // keep <br> 
+      let chars = lyric.split(/(<br>)/).map(part => {
+          if (part === '<br>') {
+              return part; 
+          }
+          // Wrap other characters in <span> for animation
+          return part.split('').map(char => `<span>${char}</span>`).join('');
+      }).join('');
+
+      lyricDiv.html(chars);
+
+      // magic
+      anime({
+          targets: 'span',
+          opacity: [0, 1],
+          duration: 1,
+          delay: anime.stagger(35),
+      });
+
+      anime({
+          targets: 'span',
+          translateX: [50, 0],
+          delay: anime.stagger(35),
+          duration: 500,
+          easing: 'easeOutExpo'
+      });
+  }
 }
